856ca3f41edf8c7cc762481f768c2a318cba1d3a
zhangliting <1109156011@qq.com>
2017-01-05 00:18:41 +0800

    finish first course

--- app.js
+++ app.js
@@ -3,57 +3,50 @@
  */
 var express = require('express')
 var path = require('path')
+var mongoose = require('mongoose')
+var Movie = require('./models/movie')
 var bodyParser = require('body-parser')
+var _ = require('underscore')
 var port = process.env.PORT || 3000
 var app = express();
+
+mongoose.connect('mongodb://localhost/imooc')
 
 app.set('views', './views/pages')
 app.set('view engine', 'jade')
 // app.use(express.bodyParser())
-app.use(bodyParser.urlencoded())
-app.use(express.static(path.join(__dirname, 'bower_components')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.locals.moment = require('moment')
 app.listen(port)
 
 console.log('movie started on port ' + port)
 
 // index page
 app.get('/', function (req, res) {
-    res.render('index', {
-        title: 'imooc 首页',
-        movies:[{
-            title: '机械战警',
-            _id: 1,
-            poster: 'http://img.bizhi.sogou.com/images/2012/04/13/236597.jpg'},
-            {
-                title: '机械战警',
-                _id: 1,
-                poster: 'http://img.bizhi.sogou.com/images/2012/04/13/236597.jpg'},
-            {
-                title: '机械战警',
-                _id: 1,
-                poster: 'http://img.bizhi.sogou.com/images/2012/04/13/236597.jpg'},
-            {
-                title: '机械战警',
-                _id: 1,
-                poster: 'http://img.bizhi.sogou.com/images/2012/04/13/236597.jpg'}
-        ]
+
+    Movie.fetch(function (err, movies) {
+        if (err) {
+            console.log(err)
+        }
+
+        res.render('index', {
+            title: 'imooc 首页',
+            movies: movies
+        })
     })
 })
 
 // detail page
 app.get('/movie/:id', function (req, res) {
-    res.render('detail', {
-        title: 'imooc 详情页',
-        movie: {
-            doctor: '何塞.帕迪利亚',
-            country: '美国',
-            title: '机械战警',
-            year: 2014,
-            poster: 'http://img.bizhi.sogou.com/images/2012/04/13/236597.jpg',
-            language: '英语',
-            flash: 'http://v.youku.com/v_show/id_XMTg5MjA2MDI2MA==.html',
-            summary: 'Test'
-        }
+
+    var id = req.params.id
+    Movie.findById(id, function (err, movie) {
+        res.render('detail', {
+            title: 'imooc' + movie.title,
+            movie: movie
+        })
     })
 })
 
@@ -61,7 +54,7 @@ app.get('/movie/:id', function (req, res) {
 app.get('/admin/movie', function (req, res) {
     res.render('admin', {
         title: 'imooc 后台录入页',
-        movie:{
+        movie: {
             title: '',
             doctor: '',
             country: '',
@@ -74,21 +67,91 @@ app.get('/admin/movie', function (req, res) {
     })
 })
 
+//admin update movie
+app.get('/admin/update/:id', function (req, res) {
+    var id = req.params.id
+
+    if(id){
+        Movie.findById(id, function (err, movie) {
+            res.render('admin',{
+                title:'imooc 后台更新页',
+                movie:movie
+            })
+
+        })
+    }
+
+})
+
+//admin post movie
+app.post('/admin/movie/new', function (req, res) {
+    var id = req.body.movie._id
+    var movieObj = req.body.movie
+    var _movie
+
+    if (id !== 'undefined') {
+        Movie.findById(id, function (err, movie) {
+            if (err) {
+                console.log(err)
+            }
+
+            _movie = _.extend(movie, movieObj)
+            _movie.save(function (err, movie) {
+                if (err) {
+                    console.log(err)
+                }
+                res.redirect('/movie/' + movie._id)
+            })
+        })
+    } else {
+        _movie = new Movie({
+            doctor: movieObj.doctor,
+            title: movieObj.title,
+            country: movieObj.country,
+            language: movieObj.language,
+            year: movieObj.year,
+            poster: movieObj.poster,
+            summary: movieObj.summary,
+            flash: movieObj.flash
+        })
+
+        _movie.save(function (err, movie) {
+            if (err) {
+                console.log(err)
+            }
+            res.redirect('/movie/' + movie._id)
+        })
+    }
+
+})
 // list page
 app.get('/admin/list', function (req, res) {
-    res.render('list', {
-        title: 'imooc 列表页',
-        movies: [{
-            title: '机械战警',
-            _id: 1,
-            doctor: '何塞.帕迪利亚',
-            country: '美国',
-            title: '机械战警',
-            year: 2014,
-            poster: 'http://img.bizhi.sogou.com/images/2012/04/13/236597.jpg',
-            language: '英语',
-            flash: 'http://v.youku.com/v_show/id_XMTg5MjA2MDI2MA==.html',
-            summary: 'Test'
-        }]
+
+    Movie.fetch(function (err, movies) {
+        if (err) {
+            console.log(err)
+        }
+
+        res.render('list', {
+            title: 'imooc 列表页',
+            movies: movies
+        })
     })
+})
+
+//list delete movie
+app.delete('/admin/list', function (req, res) {
+    var id = req.query.id
+
+    if(id){
+        Movie.remove({_id:id}, function (err, movie) {
+            if(err){
+                console.log(err)
+            }else{
+                res.json({success:1})
+            }
+
+        })
+    }
+
 })
